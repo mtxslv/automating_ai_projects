1d0e8534cf024f87652430b92c138c70f52e04ea
Mateus Assis <mateus.d.assis.silva@gmail.com>
2023-01-27 09:18:52 -0300

    feat: add virtualenv in project

--- install_project.sh
+++ install_project.sh
@@ -1,3 +1,5 @@
 # THE FOLLOWING LINES DO THE JOB INSTALLING THE POETRY PROJECT
+echo 'Local Virtualenv selected'
+poetry config virtualenvs.in-project true
 echo 'Installing project'
 poetry install
